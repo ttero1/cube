269c83ffc09f5d53b1820f37923d7900b189472f
Mirella Jaakkola <mirellajaakkola@Mirellas-MacBook-Air.local>
2025-01-27 16:21:59 +0200

    cleaned functions

--- parse_map.c
+++ parse_map.c
@@ -24,10 +24,7 @@ int	check_valid_char(char *line)
 int	validate_player_position(t_game *game)
 {
 	if (game->player.x == -1 || game->player.y == -1 || game->player.player_pos == '\0')
-	{
-		printf("No player position found\n");
 		return (0);
-	}
 	return (1);
 }
 
@@ -43,119 +40,127 @@ int	check_png_file(char *file)
 	return (0);
 }
 
-int	parse_element(char *line, t_game *game)
+int	no_text(char *line, t_game *game) 
 {
 	char	*trimmed;
 
-	if (ft_strncmp(line, "NO ", 3) == 0)
+	if (game->map.no_text)
+		return (-1);
+	trimmed = ft_strtrim(line + 3, " ");
+	if (!check_png_file(trimmed))
 	{
-		if (game->map.no_text)
-		{
-			printf("Duplicate NO texture found\n");
-			return (-1);
-		}
-		trimmed = ft_strtrim(line + 3, " ");
-		if (!check_png_file(trimmed))
-		{
-			printf("No png file\n");
-			free(trimmed);
-			return (0);
-		}
-		game->map.no_text = ft_strdup(trimmed);
 		free(trimmed);
-		if (!game->map.no_text)
-			return (-1);
-	}
-	else if (ft_strncmp(line, "SO ", 3) == 0)
-	{
-		if (game->map.so_text)
-		{
-			printf("Duplicate SO texture found\n");
-			return (-1);
-		}
-		trimmed = ft_strtrim(line + 3, " ");
-		if (!check_png_file(trimmed))
-		{
-			printf("No png file\n");
-			free(trimmed);
-			return (0);
-		}
-		game->map.so_text = ft_strdup(trimmed);
-		free(trimmed);
-		if (!game->map.so_text)
-			return (-1);
-	}
-	else if (ft_strncmp(line, "WE ", 3) == 0)
-	{
-		if (game->map.we_text)
-		{
-			printf("Duplicate WE texture found\n");
-			return (-1);
-		}
-		trimmed = ft_strtrim(line + 3, " ");
-		if (!check_png_file(trimmed))
-		{
-			printf("No png file\n");
-			free(trimmed);
-			return (0);
-		}
-		game->map.we_text = ft_strdup(trimmed);
-		free(trimmed);
-		if (!game->map.we_text)
-			return (-1);
-	}
-	else if (ft_strncmp(line, "EA ", 3) == 0)
-	{
-		if (game->map.ea_text)
-		{
-			printf("Duplicate EA texture found\n");
-			return (-1);
-		}
-		trimmed = ft_strtrim(line + 3, " ");
-		if (!check_png_file(trimmed))
-		{
-			printf("No png file\n");
-			free(trimmed);
-			return (0);
-		}
-		game->map.ea_text = ft_strdup(trimmed);
-		free(trimmed);
-		if (!game->map.ea_text)
-			return (-1);
-	}
-	else if (ft_strncmp(line, "F ", 2) == 0)
-	{
-		if (game->map.floor_color[0] != -1)
-		{
-			printf("Duplicate floor color found\n");
-			return (-1);
-		}
-		if (!parse_color(line + 2, game->map.floor_color))
-		{
-			printf("Invalid floor color format\n");
-			return (-1);
-		}
-	}
-	else if (ft_strncmp(line, "C ", 2) == 0)
-	{
-		if (game->map.ceiling_color[0] != -1)
-		{
-			printf("Duplicate ceiling color found\n");
-			return (-1);
-		}
-		if (!parse_color(line + 2, game->map.ceiling_color))
-		{
-			printf("Invalid ceiling color format\n");
-			return (-1);
-		}
-	}
-	else
-	{
-		printf("Unknown element: %s\n", line);
 		return (0);
 	}
-	printf("Success return 1 in parse_element\n");
+	game->map.no_text = ft_strdup(trimmed);
+	free(trimmed);
+	if (!game->map.no_text)
+		return (-1);
 	return (1);
+}
+
+int	so_text(char *line, t_game *game)
+{
+	char	*trimmed;
+
+	if (game->map.so_text)
+		return (-1);
+	trimmed = ft_strtrim(line + 3, " ");
+	if (!check_png_file(trimmed))
+	{
+		free(trimmed);
+		return (0);
+	}
+	game->map.so_text = ft_strdup(trimmed);
+	free(trimmed);
+	if (!game->map.so_text)
+		return (-1);
+	return (1);
+}
+
+int	we_text(char *line, t_game *game)
+{
+	char	*trimmed;
+
+	if (game->map.we_text)
+		return (-1);
+	trimmed = ft_strtrim(line + 3, " ");
+	if (!check_png_file(trimmed))
+	{
+		free(trimmed);
+		return (0);
+	}
+	game->map.we_text = ft_strdup(trimmed);
+	free(trimmed);
+	if (!game->map.we_text)
+		return (-1);
+	return (1);
+}
+
+int	ea_text(char *line, t_game *game)
+{
+	char	*trimmed;
+
+	if (game->map.ea_text)
+		return (-1);
+	trimmed = ft_strtrim(line + 3, " ");
+	if (!check_png_file(trimmed))
+	{
+		free(trimmed);
+		return (0);
+	}
+	game->map.ea_text = ft_strdup(trimmed);
+	free(trimmed);
+	if (!game->map.ea_text)
+		return (-1);
+	return (1);
+}
+
+int floor_color(char *line, t_game *game)
+{
+	if (game->map.floor_color[0] != -1)
+		return (-1);
+	if (!parse_color(line + 2, game->map.floor_color))
+		return (-1);
+	return (1);
+}
+
+int ceiling_color(char *line, t_game *game)
+{
+	if (game->map.ceiling_color[0] != -1)
+		return (-1);
+	if (!parse_color(line + 2, game->map.ceiling_color))
+		return (-1);
+	return (1);
+}
+
+int	parse_element(char *line, t_game *game)
+{
+	while (*line == ' ')
+		line++;
+	if (ft_strncmp(line, "NO ", 3) == 0)
+		return (no_text(line, game));
+	else if (ft_strncmp(line, "SO ", 3) == 0)
+		return (so_text(line, game));
+	else if (ft_strncmp(line, "WE ", 3) == 0)
+		return (we_text(line, game));
+	else if (ft_strncmp(line, "EA ", 3) == 0)
+		return (ea_text(line, game));
+	else if (ft_strncmp(line, "F ", 2) == 0)
+		return (floor_color(line, game));
+	else if (ft_strncmp(line, "C ", 2) == 0)
+		return (ceiling_color(line, game));
+	return (0);
+}
+
+void	free_rgb(char **rgb)
+{
+	int	i;
+
+	i = 0;
+	while (rgb[i])
+		free(rgb[i++]);
+	free(rgb);
 }
 
 int	parse_color(char *line, int *color)
@@ -166,33 +171,112 @@ int	parse_color(char *line, int *color)
 	rgb = ft_split(line, ',');
 	if (!rgb || !rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
 	{
-		printf("Invalid RGB format\n");
 		if (rgb)
-		{
-			i = 0;
-			while (rgb[i])
-				free(rgb[i++]);
-			free(rgb);
-		}
+			free_rgb(rgb);
 		return (0);
 	}
 	color[0] = ft_atoi(rgb[0]);
 	color[1] = ft_atoi(rgb[1]);
 	color[2] = ft_atoi(rgb[2]);
-	i = 0;
-	while (rgb[i])
-		free(rgb[i++]);
-	free(rgb);
-	if (color[0] < 0 || color[0] > 255 || color[1] < 0 || color[1] > 255 || color[2] < 0 || color[2] > 255)
-	{
-		printf("Invalid RGB color\n");
+	free_rgb(rgb);
+	if (color[0] < 0 || color[0] > 255 || color[1] < 0 \
+		|| color[1] > 255 || color[2] < 0 || color[2] > 255)
 		return (0);
+	return (1);
+}
+
+int	validate_row_edges(char *row, int row_index)
+{
+	int	len;
+	int	x;
+
+	x = 0;
+	len = ft_strlen(row);
+	while (row[x] == ' ')
+		x++;
+	if (row[x] != '1' || row[len - 1] != '1')
+		return (0);
+	return (1);
+}
+
+int	validate_row_spaces(char **map, int y, int x, t_game *game)
+{
+	int	row_len;
+	int	prev_row_len;
+	int	next_row_len;
+
+	row_len = ft_strlen(map[y]);
+	if (y < game->map.height - 1)
+		next_row_len = ft_strlen(map[y + 1]);
+	else
+		next_row_len = 0;
+	if (y != 0)
+		prev_row_len = ft_strlen(map[y - 1]);
+	else
+		prev_row_len = 0;
+	if (x > 0 && map[y][x - 1] != '1' && map[y][x - 1] != ' ')
+		return (0);
+	if (x + 1 < row_len && map[y][x + 1] != '1' && map[y][x + 1] != ' ')
+		return (0);
+	if (y > 0 && x < prev_row_len && map[y - 1][x] != '1' && map[y - 1][x] != ' ')
+		return (0);
+	if (y < game->map.height - 1 && x < next_row_len && map[y + 1][x] != '1' && map[y - 1][x] != ' ')
+		return (0);
+	return (1);
+}
+
+int	validate_top_and_bottom(char **map, t_game *game)
+{
+	int	x;
+
+	x = 0;
+	while (map[0][x] == ' ')
+		x++;
+	while (x < ft_strlen(map[0]))
+	{
+		if (map[0][x] != '1')
+			return (0);
+		x++;
+	}
+	x = 0;
+	while (map[game->map.height - 1][x] == ' ')
+		x++;
+	while (x < ft_strlen(map[game->map.height - 1]))
+	{
+		if (map[game->map.height - 1][x] != '1')
+			return (0);
+		x++;
 	}
 	return (1);
 }
 
-
 int	validate_map(t_game *game)
+{
+	char	**map;
+	int		y;
+	int		x;
+
+	map = game->map.points;
+	y = 0;
+	while (y < game->map.height)
+	{
+		if (!validate_row_edges(map[y], y))
+			return (0);
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == ' ' && !validate_row_spaces(map, y, x, game))
+				return (0);
+			x++;
+		}
+		y++;
+	}
+	if (!validate_top_and_bottom(map, game))
+		return (0);
+	return (1);
+}
+
+/*int	validate_map(t_game *game)
 {
 	char	**map;
 	int		y;
@@ -200,13 +284,9 @@ int	validate_map(t_game *game)
 	int		row_len;
 	int		next_row_len;
 	int		prev_row_len;
-	//int		space_top;
-	//int		space_bottom;
 
 	map = game->map.points;
 	y = 0;
-	//space_top = 0;
-	//space_bottom = 0;
 	while (y < game->map.height)
 	{
 		x = 0;
@@ -227,10 +307,6 @@ int	validate_map(t_game *game)
 		{
 			if (map[y][x] == ' ')
 			{
-				//if (map[0][x])
-				//	space_top = 1;
-				//if (map[game->map.height - 1][x])
-				//	space_bottom = 1;
 				if (x > 0 && map[y][x - 1] != '1' && map[y][x - 1] != ' ')
 				{
 					printf("In left space not surrounded by wall\n");
@@ -281,7 +357,7 @@ int	validate_map(t_game *game)
 		x++;
 	}
 	return (1);
-}
+}*/
 
 void	trim_newline(char *line)
 {
@@ -306,10 +382,7 @@ int	save_line(char *line, t_game *game, int row)
 		if (ft_strchr("NSEW", line[x]))
 		{
 			if (game->player.x != -1)
-			{
-				printf("Multiple player positions found\n");
 				return (0);
-			}
 			game->player.x = x;
 			game->player.y = row;
 			game->player.player_pos = line[x];
@@ -328,14 +401,10 @@ int	count_map_rows(const char *file)
 	count = 0;
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
-	{
-		printf("Could not open file %s\n", file);
 		return (-1);
-	}
 	line = get_next_line(fd);
 	if (!line)
 	{
-		printf("File %s is empty\n", file);
 		close(fd);
 		return (0);
 	}
@@ -351,37 +420,127 @@ int	count_map_rows(const char *file)
 	return(count);
 }
 
-/*int	init_map(t_map *map)
+int	init_map(const char *file, t_game *game)
 {
-	map->points = NULL;
-	map->width = 0;
-	map->height = 0;
-	map->no_text = NULL;
-	map->so_text = NULL;
-	map->we_text = NULL;
-	map->ea_text = NULL;
-	map->floor_color = malloc(3 * sizeof(int));
-	if (!map->floor_color)
+	int	malloc_size;
+	int	j;
+
+	malloc_size = count_map_rows(file);
+	if (malloc_size <= 0)
 		return (0);
-	map->ceiling_color = malloc(3 * sizeof(int));
-	if (!map->ceiling_color)
+	game->map.points = malloc((malloc_size + 1) * sizeof(char *));
+	if (!game->map.points)
+		return (0);
+	j = 0;
+	while (j <= malloc_size)
 	{
-		free(map->floor_color);
-		return (0);
+		game->map.points[j] = NULL;
+		j++;
 	}
-	map->floor_color[0] = -1;
-	map->ceiling_color[0] = -1;
 	return (1);
 }
 
-void	init_player(t_player *player)
+int	process_content(char *line, t_game *game, int *row, int *map)
 {
-	player->x = -1;
-	player->y = -1;
-	player->player_pos = '\0';
-}*/
+	if (game->map.floor_color[0] != -1 && game->map.ceiling_color[0] != -1)
+	{
+		if (!check_valid_char(line))
+			return (0);
+	}
+	if (!*map && parse_element(line, game) == -1)
+		return (0);
+	if (!*map && !parse_element(line, game))
+	{
+		if (!game->map.no_text || !game->map.so_text || !game->map.we_text || !game->map.ea_text)
+			return (0);
+		*map = 1;
+		return (save_line(line, game, (*row)++));
+	}
+	else if (*map)
+		return (save_line(line, game, (*row)++));
+	return (1);
+}
+
+int	process_map(char *line, t_game *game, int fd, int *row)
+{
+	int	map;
+
+	map = 0;
+	while (line)
+	{
+		trim_newline(line);
+		if (line[0] == '\0')
+		{
+			free(line);
+			if (map)
+				return (0);
+			line = get_next_line(fd);
+			continue ;
+		}
+		if (!process_content(line, game, row, &map))
+		{
+			free(line);
+			return (0);
+		}
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (!map)
+		return (0);
+	return (1);
+}
+
+int	finalize_map(t_game *game, int row)
+{
+	int	j;
+	int	len;
+
+	game->map.height = row;
+	game->map.points[row] = NULL;
+	game->map.width = 0;
+	j = 0;
+	while (j < row)
+	{
+		len = ft_strlen(game->map.points[j]);
+		if (len > game->map.width)
+			game->map.width = len;
+		j++;
+	}
+	if (!validate_map(game))
+		return (0);
+	if (!validate_player_position(game))
+		return (0);
+	return (1);
+}
 
 int	parse_map(const char *file, t_game *game)
+{
+	int		fd;
+	char	*line;
+	int		row;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	if (!init_map(file, game))
+		return (0);
+	line = get_next_line(fd);
+	if (!line)
+	{
+		close(fd);
+		return (0);
+	}
+	row = 0;
+	if (!process_map(line, game, fd, &row))
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return (finalize_map(game, row));
+}
+
+/*int	parse_map(const char *file, t_game *game)
 {
 	int		fd;
 	char	*line;
@@ -394,7 +553,6 @@ int	parse_map(const char *file, t_game *game)
 	malloc_size = count_map_rows(file);
 	if (malloc_size <= 0)
 		return (0);
-
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		return (0);
@@ -406,9 +564,6 @@ int	parse_map(const char *file, t_game *game)
 	}
 	row = 0;
 	map = 0;
-	//if (!init_map(&game->map))
-	//	return (0);
-	//init_player(&game->player);
 	game->map.points = malloc((malloc_size + 1) * sizeof(char *));
 	if (!game->map.points)
 		return (0);
@@ -485,4 +640,5 @@ int	parse_map(const char *file, t_game *game)
 	if (!validate_player_position(game))
 		return (0);
 	return (1);
-}
+}*/
+

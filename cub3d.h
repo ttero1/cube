41445cb81dee48528154eb03c17bc1536a580270
Teemu Tero <ttero@c1r5p13.hive.fi>
2025-02-03 10:42:27 +0200

    norminette fix

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ttero <ttero@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:00:17 by ttero             #+#    #+#             */
-/*   Updated: 2025/02/01 15:41:05 by ttero            ###   ########.fr       */
+/*   Updated: 2025/02/03 10:38:28 by ttero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # define HEIGHT 800
 # define SPEED 0.2
 # define SPEED_RAD 0.03
-# define BLOCK_SIZE 30
+# define BLOCK_SIZE 10
 # define PI 3.141592653589793
 # define RAD 0.01745329251
 
--- draw_minimap.c
+++ draw_minimap.c
@@ -6,16 +6,19 @@
 /*   By: ttero <ttero@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:30:42 by ttero             #+#    #+#             */
-/*   Updated: 2025/02/02 21:11:22 by ttero            ###   ########.fr       */
+/*   Updated: 2025/02/03 10:40:45 by ttero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_colors(uint32_t *color_block, uint32_t *color_empty)
+void	init_block_values(uint32_t *color_block, uint32_t *color_empty,
+	int *z, int *a)
 {
 	*color_block = ft_pixel(200, 100, 0, 255);
 	*color_empty = ft_pixel(255, 255, 255, 255);
+	*z = -1;
+	*a = 0;
 }
 
 void	paint_block(t_game *game, int x, int y, int flag)
@@ -25,12 +28,10 @@ void	paint_block(t_game *game, int x, int y, int flag)
 	uint32_t	color_block;
 	uint32_t	color_empty;
 
-	init_colors(&color_block, &color_empty);
-	z = -1;
-	a = 0;
+	init_block_values(&color_block, &color_empty, &z, &a);
 	x *= 5;
 	y *= 5;
-	if (x > WIDTH / 2 || y > HEIGHT / 2) 
+	if (x > WIDTH / 2 || y > HEIGHT / 2)
 		return ;
 	while (z++ < 5)
 	{
@@ -57,7 +58,8 @@ void	draw_player(t_game *game)
 
 	x = 0;
 	y = 0;
-	if (game->x_pos * 5 / BLOCK_SIZE > WIDTH / 2 || game->y_pos * 5 / BLOCK_SIZE > HEIGHT / 2)
+	if (game->x_pos * 5 / BLOCK_SIZE > WIDTH / 2
+		|| game->y_pos * 5 / BLOCK_SIZE > HEIGHT / 2)
 		return ;
 	color = ft_pixel(0, 0, 0, 255);
 	while (y < 3)
